6c3243cfa3cc32cb97552a6c1b8808af02c50666
AlenS <alenskupnjak@yahoo.com>
2020-02-09 21:22:33 +0100

    011

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipe
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { RecipeServise } from './recipes/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.services';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [RecipeServise, ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeServise} from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -8,16 +9,13 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() odabraniRecept = new EventEmitter<Recipe>();
-  recipes: Recipe [] = [
-    new Recipe(' A test 1', ' Opis recepta 1', 'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
-    new Recipe(' A test 2', ' Opis recepta 2', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe(' A test 3', ' Opis recepta 3', 'https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'),
-    new Recipe(' A test 4', ' Opis recepta 4', 'https://get.pxhere.com/photo/burger-delicious-dinner-fast-food-food-junk-food-lunch-meal-mouth-watering-onion-rings-1567565.jpg')
-  ];
+  recipes: Recipe [] = [];
 
-  constructor() { }
+
+  constructor(private getRecipe: RecipeServise) { }
 
   ngOnInit() {
+    this.recipes = this.getRecipe.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
