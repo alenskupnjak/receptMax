efdb1fcc7419797259eaed86c7985903cac9d221
AlenS <alenskupnjak@yahoo.com>
2020-02-10 09:32:54 +0100

    016

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from '../recipes/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.models';
 
 export class RecipeServise {
 
@@ -7,13 +8,20 @@ export class RecipeServise {
 
   private recipes: Recipe [] = [
     new Recipe(' A test 1', ' Opis recepta 1',
-    'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
-    new Recipe(' A test 2', ' Opis recepta 2',
-    'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png'),
-    new Recipe(' A test 3', ' Opis recepta 3',
-    'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg'),
-    new Recipe(' A test 4', ' Opis recepta 4',
-    'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg')
+    'https://cdn.pixabay.com/photo/2018/10/31/12/37/healthy-food-3785722_960_720.jpg',
+    [
+      new Ingredient ('meat', 1),
+      new Ingredient('pomfri', 3)
+    ]
+    ),
+    new Recipe(' Big Mac', ' veliki obrok',
+    'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png',
+    [
+      new Ingredient ('meso', 1),
+      new Ingredient('luk', 4),
+      new Ingredient('salata', 2)
+    ]),
+
   ];
 
 
