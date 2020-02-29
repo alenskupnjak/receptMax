708e0d72b42d55ec6f1ca62e9e56fa3253cd7e38
AlenS <alenskupnjak@yahoo.com>
2020-02-29 15:07:54 +0100

    062 dodan inceptcetor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -25,31 +25,45 @@ export class DataStorageService {
 
 
   usnimiRecepteIzBaze() {
-    return this.authService.user.pipe(
-      take(1),
-      exhaustMap(user => {
-        console.log('**********');
-        console.log(user);
-      return this.http.get<Recipe[]>
-      ('https://httpmax-8a9bc.firebaseio.com/recipes.json'
-      , {
-        params: new HttpParams().set('auth', user.token)
-        }
-        );
-    }),
-    map(recipes => {
-      return recipes.map( recipe => {
-        // Ako nema namirnica u receptu u bazi, ovdje dodajemo prazno polje
-        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-      });
-    }), tap(recipes => {
-      this.recipeService.setRecipe(recipes);
-    })
-    ).subscribe(response =>{
-      console.log(response);
-    });
-
+    return this.http.get<Recipe[]>
+        ('https://httpmax-8a9bc.firebaseio.com/recipes.json',
+        ).pipe(
+          map(recipes => {
+            return recipes.map( recipe => {
+              // Ako nema namirnica u receptu u bazi, ovdje dodajemo prazno polje
+              return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+            });
+          }), tap(recipes => {
+            this.recipeService.setRecipe(recipes);
+          })
+          ).subscribe(response => {
+            console.log(response);
+          });
 
   }
 
 }
+
+
+// usnimiRecepteIzBaze() {
+//   return this.authService.user.pipe(
+//     take(1),
+//     exhaustMap(user => {
+//       return this.http.get<Recipe[]>
+//       ('https://httpmax-8a9bc.firebaseio.com/recipes.json',
+//         {
+//           params: new HttpParams().set('auth', user.token)
+//         }
+//       );
+//   }),
+//   map(recipes => {
+//     return recipes.map( recipe => {
+//       // Ako nema namirnica u receptu u bazi, ovdje dodajemo prazno polje
+//       return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+//     });
+//   }), tap(recipes => {
+//     this.recipeService.setRecipe(recipes);
+//   })
+//   ).subscribe(response => {
+//     console.log(response);
+//   });
